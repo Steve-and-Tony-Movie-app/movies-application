437e22e5a4f8e2fe106508fdd27484a0454d51d1
stephencisler <stephencisler@gmail.com>
2017-11-13 16:13:20 -0600

    added html builder and can submit a new movie for database, which will reload on screen

--- src/index.js
+++ src/index.js
@@ -1,19 +1,74 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
 
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const $ = require('jquery');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+// creating the html for each individual movie in the array
+const movieHTMLbuilder = (movie) => {
+  let html = `<h4 class="col-xs-6">`;
+    html += `${movie.title}`;
+    html += `</h4><h4 class="col-xs-6">`;
+    html += `Rating: ${movie.rating}`;
+    html += `</h4>`;
+    return html;
+};
+
+
+// starts the html build for the whole movie array
+const movieArrayHTML = (arrayOfMovies) => {
+  let movieListHTML = ``;
+  arrayOfMovies.forEach((element) => {
+    movieListHTML += movieHTMLbuilder(element);
   });
+  return movieListHTML;
+};
+
+
+// adding eventlistener to new movie button
+const newMovieAdder = () => {
+    const submitButton = $(`#submitBtn`);
+    const movieField = $(`#movieTitleInput`);
+    const ratingField = $(`#movieRatingInput`);
+    submitButton.click(() => {
+        let newTitle = movieField.val();
+        let newRating = ratingField.val();
+        console.log(newTitle + " " + newRating);
+        uploadNewMovie(newTitle, newRating);
+    });
+};
+
+// function to submit new movie to API server
+const uploadNewMovie = (title, rating) => {
+
+  const newMovie = {title: title, rating: rating};
+  const url = './api/movies';
+  const options = {
+    method: 'POST',
+      headers: {
+      'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newMovie)
+  };
+  fetch(url, options)
+      .then((response) => {
+        console.log('post worked');
+        return response.json();
+      }).then((data) => {
+    console.log(data);
+      })
+      .catch(() => { alert('the upload failed')});
+};
+
+// gets the initial ajax request for movies on page load
+getMovies().then((movies) => {
+  $(`#display-movies`).html(movieArrayHTML(movies));
+  newMovieAdder();
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
