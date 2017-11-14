834da211fd8147c7aa6dd5ec94ed0c9a794cdb39
stephencisler <stephencisler@gmail.com>
2017-11-14 12:09:32 -0600

    Edit movie requests work and display on screen. need to add delete functionality

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@
 const {getMovies} = require('./api.js');
 const $ = require('jquery');
 
+//----------------------------------------------------
+
 
 // creating the html for each individual movie in the array
 const movieHTMLbuilder = (movie) => {
@@ -16,10 +18,10 @@ const movieHTMLbuilder = (movie) => {
     html += `</h4><h4 class="col-xs-4">`;
     html += `Rating: ${movie.rating}`;
     html += `</h4>`;
-    html += `<div class="col-xs-4"><button type="button" id="editBtn${movie.id}">Submit Edited Movie</button></div> </div></div>`;
+    html += `<div class="col-xs-4"><button type="button" id="editBtn${movie.id}">Toggle Edit</button><button type="button" id="deleteBtn${movie.id}">DELETE</button></div>`;
+    html += `<div class="col-xs-12" id="editInputs${movie.id}"></div></div></div>`;
     return html;
 };
-
 
 // starts the html build for the whole movie array
 const movieArrayHTML = (arrayOfMovies) => {
@@ -30,6 +32,8 @@ const movieArrayHTML = (arrayOfMovies) => {
   return movieListHTML;
 };
 
+//-----------------------------------------------------
+// New Movie Adding functions
 
 // adding eventlistener to new movie button
 const newMovieAdder = () => {
@@ -39,7 +43,6 @@ const newMovieAdder = () => {
     submitButton.click(() => {
         let newTitle = movieField.val();
         let newRating = ratingField.val();
-        console.log(newTitle + " " + newRating);
         uploadNewMovie(newTitle, newRating);
     });
 };
@@ -57,19 +60,18 @@ const uploadNewMovie = (title, rating) => {
       body: JSON.stringify(newMovie)
   };
   fetch(url, options)
-      .then((response) => {
-        console.log('post worked');
-        return response.json();
-      }).then((data) => {
-      $(`#display-movies`).html(movieArrayHTML(movies));
-    console.log(data);
+      .then(() => {
+      fetchingMovieList();
       })
       .catch(() => { alert('the upload failed')});
 };
-// function Delete or Put
 
+//------------------------------------------------------
+//Edit Movie Functions
+
+
+// function to submit PUT request to api server to edit existing movies
 const editNewMovie = (title, rating, id) => {
-
     const upDatedMovie = {title: title, rating: rating};
     const url = `./api/movies/${id}`;
     const options = {
@@ -80,32 +82,53 @@ const editNewMovie = (title, rating, id) => {
         body: JSON.stringify(upDatedMovie)
     };
     fetch(url, options)
-        .then((response) => {
-            console.log('put worked');
-            return response.json();
-        }).then((data) => {
-        $(`#display-movies`).html(movieArrayHTML(movies));
-        console.log(data);
-    })
-        .catch(() => { alert('the upload failed')});
+        .then(() => {
+        console.log(`new edit function is firing`);
+        fetchingMovieList();
+        })
+        .catch(() => { console.log('the upload failed')});
 };
 
-//
+// This adds listeners to "Toggle Edit" buttons and generates new text fields to add movie changes with
  const addToggleBtn = (movies) =>{
      let arrayLength = movies.length;
      for (let i =1; i<= arrayLength; i++){
-         $(`#editBtn${i}`).click();
+         $(`#editBtn${i}`).click(() => {
+             let editTextHTML = `<div class="row">`;
+             editTextHTML += `<div class="row"><div class="col-xs-4">`;
+             editTextHTML += `<input type="text" value="${movies[(i-1)].title}" id="movieTitle${i}">`;
+             editTextHTML += `</div><div class="col-xs-4">`;
+             editTextHTML += `<input type="number" value="${movies[(i-1)].rating}" id="movieRating${i}">`;
+             editTextHTML += `</div><div class="col=xs-4">`;
+             editTextHTML += `<button type="button" id="submitEdit${i}">Submit Edit</button>`;
+             editTextHTML += `<div></div></div>`;
+             $(`#editInputs${i}`).html(editTextHTML);
+             $(`#submitEdit${i}`).click(() => {
+                 console.log("submit edit button is activating");
+                 let newTitle = $(`#movieTitle${i}`).val();
+                 let newRating = $(`#movieRating${i}`).val();
+                 editNewMovie(newTitle, newRating, movies[i-1].id);
+             })
+         });
      }
-
  };
 
+// --------------------------------------------------
 
-// gets the initial ajax request for movies on page load
-getMovies().then((movies) => {
-  $(`#display-movies`).html(movieArrayHTML(movies));
-  newMovieAdder();
+// This gets updated list of movies from the server and displays them on the screen
+const fetchingMovieList = () => {
+    $(`#display-movies`).html(`<h1>Movies are Loading...</h1>`);
+    getMovies().then((movies) => {
+      $(`#display-movies`).html(movieArrayHTML(movies));
+        addToggleBtn(movies);
+    }).catch((error) => {
+      alert('Oh no! Something went wrong.\nCheck the console for details.');
+      console.log(error);
+    })
+};
 
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
-});
+//----------------------------------------------------
+
+// Here we are calling the functions defined above
+fetchingMovieList();
+newMovieAdder();
